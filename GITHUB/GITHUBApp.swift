5fac0d1222625dc509391506003241c1327cef68
Naayeshaa <Naayeshaa@gmail.com>
2023-06-26 15:28:02 -0400

    Initial Commit

---
+++ GITHUB/GITHUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITHUBApp.swift
+//  GITHUB
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GITHUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
